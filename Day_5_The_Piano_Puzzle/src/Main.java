6a65ca817d02e8c31289364f4ab43a18d74e5325
Pedro <pedrovlt@proton.me>
2025-10-26 12:21:36 +0100

    improvement of code quality

--- Day_5_The_Piano_Puzzle/src/Main.java
+++ Day_5_The_Piano_Puzzle/src/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
             for(int i=0;i<fileContent.length()-7;i++){
                 String sequences = fileContent.substring(i,i+7);
-                boolean contains = musicalNotes.chars().allMatch(c -> sequences.indexOf(c) >= 0);
+                boolean contains = musicalNotes.chars().allMatch(position -> sequences.indexOf(position) >= 0);
                 if (contains){
                     System.out.println(sequences);
                 }
